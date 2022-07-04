6e2b22cf47484d4e25e8452b0df1310a155bd451
Nik Sharpio <niksharpkings@gmail.com>
2022-07-04 03:07:10 -0700

    test

---
+++ README.md
@@ -0,0 +1,14 @@
+# quiz-it-up
+## Description
+quiz-it-up project UCLAx Student Project,using basic Javascript and webAPI, with simple css and Html coding. include timers and score keeping, making DOM elements through JS instead of HTML content saved to local storage. 
+JS Objects and Arrays 
+Pushed Objects into Arrays and then save these into localStorage.
+Used JSON.stringify and JSON.parse methods to save and access the localStorage.
+
+## Features
+The quiz features include functioning buttons with JavaScript, Start, submit, go back, highscore, clear highscore. 
+Has a high scores table. Additionally, the 'View high scores' hyperlink takes the user to the high score table regardless of where they are in the quiz.
+The quiz has 5 questions and choices goes to a selected choose of selection arrays.
+
+### Deployed Link
+https://niksharpkings.github.io/quiz-it-up
---
+++ assets/js/questionslist.js
@@ -0,0 +1,52 @@
+const questionsArray = [
+  {
+    numberArray: 1,
+    question: "Question 1: Commonly used data types DO NOT include:",
+    answers: [
+      { option: "strings", correct: false },
+      { option: "booleans", correct: false },
+      { option: "alerts", correct: true },
+      { option: "numbers", correct: false }
+    ]
+  },
+  {
+    numberArray: 2,
+    question: "Question 2: The condition of an if/else statement is enclosed with",
+    answers: [
+      { option: "quotes", correct: false },
+      { option: "curly brackets", correct: true },
+      { option: "parentheses", correct: false },
+      { option: "square brackets", correct: false }
+    ]
+  },
+  {
+    numberArray: 3,
+    question: "Question 3: Arrays in JavaScript can be used to store ____.",
+    answers: [
+      { option: "numbers and strings", correct: false },
+      { option: "other arrays", correct: false },
+      { option: "booleans", correct: false },
+      { option: "all of the above", correct: true }
+    ]
+  },
+  {
+    numberArray: 4,
+    question: "Question 4: String values must be enclosed within ____ when being assigned to variables.",
+    answers: [
+      { option: "commas", correct: false },
+      { option: "curly brackets", correct: false },
+      { option: "quotes", correct: true },
+      { option: "parentheses", correct: false }
+    ]
+  },
+  {
+    numberArray: 5,
+    question: "Question 5: A very useful tool used during development and debugging for printing content to the debugger is:",
+    answers: [
+      { option: "terminal", correct: false },
+      { option: "for loops", correct: false },
+      { option: "console.log()", correct: true },
+      { option: "JavaScript", correct: false }
+    ]
+  }
+];
